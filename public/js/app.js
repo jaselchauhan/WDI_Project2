49f814ddc2da5297ad21e8d5163a9dcf577a0175
Jasel Chauhan <jasel.chauhan@gmail.com>
2016-02-22 22:07:29 +0000

    tried adding google maps, although not successful just yet. committing nonetheless

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,24 @@
 console.log("vanilla js loaded from public/js/app.js");
 
-$( document ).ready(function() {
+// $( document ).ready(function() {
     console.log( "jQuery ready from app.js!" );
-});
+
+
+
+
+function initialize() {
+  var mapCanvas = document.getElementById('map');
+  var mapOptions = {
+    center: new google.maps.LatLng(15.0112, 74.0217),
+    zoom: 17,
+    mapTypeId: google.maps.MapTypeId.HYBRID
+  }
+  var map = new google.maps.Map(mapCanvas, mapOptions)
+  google.maps.event.addDomListener(window, 'load', initialize);
+}
+
+
+
+
+
+// });
