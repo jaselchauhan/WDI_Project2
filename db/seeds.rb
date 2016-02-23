32a2b789dab32d62b491eee274ed9a87f76e452f
Jasel Chauhan <jasel.chauhan@gmail.com>
2016-02-23 00:19:21 +0000

    added more data to seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,7 @@ Comment.destroy_all
     first_name: "pai",
     last_name: "mei",
     email: "pai@mei.com",
-    image: "http://www.fillmurray.com/200/300",
+    image: "http://www.fillmurray.com/300/300",
     bio: "Sustainable tumblr meh echo park. Mlkshk skateboard beard pabst hammock, tumblr chicharrones cliche banjo 90's sustainable man bun williamsburg pour-over. Skateboard chartreuse chia, cold-pressed before they sold out offal whatever plaid portland. Health goth wolf mixtape artisan chillwave, taxidermy cornhole. Cronut quinoa stumptown pork belly, cray chicharrones shabby chic drinking vinegar. Aesthetic pour-over shabby chic, humblebrag polaroid listicle locavore poutine everyday carry tattooed actually tilde. Cred four loko bespoke fixie kinfolk fashion axe heirloom hashtag, lo-fi cliche crucifix farm-to-table.
 
 Vice meh green juice, +1 shabby chic vinyl williamsburg venmo mumblecore. Man bun VHS marfa mumblecore 3 wolf moon XOXO jean shorts, portland migas small batch. Listicle williamsburg viral microdosing fingerstache, blog pabst drinking vinegar retro craft beer bushwick cornhole tacos plaid. Chartreuse letterpress pour-over, tofu microdosing lo-fi food truck typewriter wolf organic before they sold out selvage ethical tumblr keffiyeh. Cold-pressed four dollar toast salvia affogato sartorial pour-over, irony actually kitsch franzen polaroid thundercats squid viral. Green juice slow-carb flexitarian bicycle rights, viral semiotics fanny pack kitsch tofu whatever cronut. Cronut cold-pressed YOLO skateboard fixie.", 
@@ -26,13 +26,54 @@ Vice meh green juice, +1 shabby chic vinyl williamsburg venmo mumblecore. Man bu
     first_name: "Patient",
     last_name: "Buffalo",
     email: "walle@gmail.com",
-    image: "http://www.fillmurray.com/201/301",
+    image: "http://www.fillmurray.com/300/300",
     bio: "Sustainable tumblr meh echo park. Mlkshk skateboard beard pabst hammock, tumblr chicharrones cliche banjo 90's sustainable man bun williamsburg pour-over. Skateboard chartreuse chia, cold-pressed before they sold out offal whatever plaid portland. Health goth wolf mixtape artisan chillwave, taxidermy cornhole. Cronut quinoa stumptown pork belly, cray chicharrones shabby chic drinking vinegar. Aesthetic pour-over shabby chic, humblebrag polaroid listicle locavore poutine everyday carry tattooed actually tilde. Cred four loko bespoke fixie kinfolk fashion axe heirloom hashtag, lo-fi cliche crucifix farm-to-table.
 
 Vice meh green juice, +1 shabby chic vinyl williamsburg venmo mumblecore. Man bun VHS marfa mumblecore 3 wolf moon XOXO jean shorts, portland migas small batch. Listicle williamsburg viral microdosing fingerstache, blog pabst drinking vinegar retro craft beer bushwick cornhole tacos plaid. Chartreuse letterpress pour-over, tofu microdosing lo-fi food truck typewriter wolf organic before they sold out selvage ethical tumblr keffiyeh. Cold-pressed four dollar toast salvia affogato sartorial pour-over, irony actually kitsch franzen polaroid thundercats squid viral. Green juice slow-carb flexitarian bicycle rights, viral semiotics fanny pack kitsch tofu whatever cronut. Cronut cold-pressed YOLO skateboard fixie.", 
     user_type: "wall_owner",
     password: "patient",
-  }
+  },
+    
+    {
+      username: "renaissance_man", 
+      first_name: "leonardo",
+      last_name: "di vinci",
+      email: "goldenratio@gmail.com",
+      image: "http://www.fillmurray.com/300/300",
+      bio: "Sustainable tumblr meh echo park. Mlkshk skateboard beard pabst hammock, tumblr chicharrones cliche banjo 90's sustainable man bun williamsburg pour-over. Skateboard chartreuse chia, cold-pressed before they sold out offal whatever plaid portland. Health goth wolf mixtape artisan chillwave, taxidermy cornhole. Cronut quinoa stumptown pork belly, cray chicharrones shabby chic drinking vinegar. Aesthetic pour-over shabby chic, humblebrag polaroid listicle locavore poutine everyday carry tattooed actually tilde. Cred four loko bespoke fixie kinfolk fashion axe heirloom hashtag, lo-fi cliche crucifix farm-to-table.
+
+  Vice meh green juice, +1 shabby chic vinyl williamsburg venmo mumblecore. Man bun VHS marfa mumblecore 3 wolf moon XOXO jean shorts, portland migas small batch. Listicle williamsburg viral microdosing fingerstache, blog pabst drinking vinegar retro craft beer bushwick cornhole tacos plaid. Chartreuse letterpress pour-over, tofu microdosing lo-fi food truck typewriter wolf organic before they sold out selvage ethical tumblr keffiyeh. Cold-pressed four dollar toast salvia affogato sartorial pour-over, irony actually kitsch franzen polaroid thundercats squid viral. Green juice slow-carb flexitarian bicycle rights, viral semiotics fanny pack kitsch tofu whatever cronut. Cronut cold-pressed YOLO skateboard fixie.", 
+      user_type: "artist",
+      password: "password",
+    },
+
+      
+      {
+        username: "drunkhippo", 
+        first_name: "drunk",
+        last_name: "hippo",
+        email: "walle2@gmail.com",
+        image: "http://www.fillmurray.com/300/300",
+        bio: "Sustainable tumblr meh echo park. Mlkshk skateboard beard pabst hammock, tumblr chicharrones cliche banjo 90's sustainable man bun williamsburg pour-over. Skateboard chartreuse chia, cold-pressed before they sold out offal whatever plaid portland. Health goth wolf mixtape artisan chillwave, taxidermy cornhole. Cronut quinoa stumptown pork belly, cray chicharrones shabby chic drinking vinegar. Aesthetic pour-over shabby chic, humblebrag polaroid listicle locavore poutine everyday carry tattooed actually tilde. Cred four loko bespoke fixie kinfolk fashion axe heirloom hashtag, lo-fi cliche crucifix farm-to-table.
+
+    Vice meh green juice, +1 shabby chic vinyl williamsburg venmo mumblecore. Man bun VHS marfa mumblecore 3 wolf moon XOXO jean shorts, portland migas small batch. Listicle williamsburg viral microdosing fingerstache, blog pabst drinking vinegar retro craft beer bushwick cornhole tacos plaid. Chartreuse letterpress pour-over, tofu microdosing lo-fi food truck typewriter wolf organic before they sold out selvage ethical tumblr keffiyeh. Cold-pressed four dollar toast salvia affogato sartorial pour-over, irony actually kitsch franzen polaroid thundercats squid viral. Green juice slow-carb flexitarian bicycle rights, viral semiotics fanny pack kitsch tofu whatever cronut. Cronut cold-pressed YOLO skateboard fixie.", 
+        user_type: "wall_owner",
+        password: "password",
+      }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 ].each do |user|
   User.create!(user)
 end
