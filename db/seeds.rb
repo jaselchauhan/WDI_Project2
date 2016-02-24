fe8e54b53ed2d9c0241259aa873d433c53e208ad
Jasel Chauhan <jasel.chauhan@gmail.com>
2016-02-24 19:19:43 +0000

    started syling images. atm all the same size

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,8 +15,8 @@ Comment.destroy_all
     username: "paimei",
     first_name: "pai",
     last_name: "mei",
-    email: "pai@mei.com",
-    image: "http://www.fillmurray.com/300/300",
+    email: "paimei@gmail.com",
+    image: "http://images.uncyc.org/pt/thumb/4/40/Pai_Mei.jpg/250px-Pai_Mei.jpg",
     bio: "Sustainable tumblr meh echo park. Mlkshk skateboard beard pabst hammock, tumblr chicharrones cliche banjo 90's sustainable man bun williamsburg pour-over. Skateboard chartreuse chia, cold-pressed before they sold out offal whatever plaid portland. Health goth wolf mixtape artisan chillwave, taxidermy cornhole. Cronut quinoa stumptown pork belly, cray chicharrones shabby chic drinking vinegar. Aesthetic pour-over shabby chic, humblebrag polaroid listicle locavore poutine everyday carry tattooed actually tilde. Cred four loko bespoke fixie kinfolk fashion axe heirloom hashtag, lo-fi cliche crucifix farm-to-table.
 
 Vice meh green juice, +1 shabby chic vinyl williamsburg venmo mumblecore. Man bun VHS marfa mumblecore 3 wolf moon XOXO jean shorts, portland migas small batch. Listicle williamsburg viral microdosing fingerstache, blog pabst drinking vinegar retro craft beer bushwick cornhole tacos plaid. Chartreuse letterpress pour-over, tofu microdosing lo-fi food truck typewriter wolf organic before they sold out selvage ethical tumblr keffiyeh. Cold-pressed four dollar toast salvia affogato sartorial pour-over, irony actually kitsch franzen polaroid thundercats squid viral. Green juice slow-carb flexitarian bicycle rights, viral semiotics fanny pack kitsch tofu whatever cronut. Cronut cold-pressed YOLO skateboard fixie.", 
